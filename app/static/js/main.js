069e8905f665b04e7ac357ca463ce27e9b96323a
fast-foot <alex.chudovsky@gmail.com>
2016-11-07 16:34:20 +0300

    Implemented SRM form.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,23 +1,57 @@
 $(document).ready(function () {
-    $('#btn-add-product').click(function () {
-        var startYear = parseInt($('#start-year').val());
-        var startMonth = parseInt($('#start-month').val());
-        var endYear = parseInt($('#end-year').val());
-        var endMonth = parseInt($('#end-month').val());
+    $('#crm-form #btn-add-product').click(function () {
+        var startYear = parseInt($('#crm-form #start-year').val());
+        var startMonth = parseInt($('#crm-form #start-month').val());
+        var endYear = parseInt($('#crm-form #end-year').val());
+        var endMonth = parseInt($('#crm-form #end-month').val());
 
         var plansPeriods = generatePlanPeriods(startYear, startMonth, endYear, endMonth);
 
         var productRow = "<tr>";
 
-        productRow += "<td>" + ($('#crm-form #products-info-table > tbody > tr').length + 1) + "</td>";
-        productRow += getProductDetailsCellData();
-        productRow += "<td><div>" + buildStrategicPlanTable(plansPeriods) + "</div></td>";
-        productRow += "<td><div>" + buildPerspectivePlanTable(plansPeriods) + "</div></td>";
-        productRow += "<td><div>" + buildOperativePlanTable(plansPeriods) + "</div></td>";
+        productRow += "<td class='col-lg-1'>" + ($('#crm-form #products-info-table > tbody > tr').length + 1) + "</td>";
+        productRow += getProductDetailsForCRMCellData();
+        productRow +=
+            "<td class='col-lg-8'>" +
+                "<h4 class='centeredText'>Strategic</h4>" +
+                "<div class='tableDiv'>" + buildStrategicPlanTable(plansPeriods) + "</div>" +
+                "<h4 class='centeredText'>Perspective</h4>" +
+                "<div class='tableDiv'>" + buildPerspectivePlanTable(plansPeriods) + "</div>" +
+                "<h4 class='centeredText'>Operative</h4>" +
+                "<div class='tableDiv'>" + buildOperativePlanTable(plansPeriods) + "</div>" +
+            "</td>";
 
-        productRow += "<td></td></tr>";
+        productRow += "</tr>";
 
         $('#crm-form #products-info-table > tbody').append(productRow);
+
+    });
+
+    $('#srm-form #btn-add-product').click(function () {
+        var startYear = parseInt($('#srm-form #start-year').val());
+        var startMonth = parseInt($('#srm-form #start-month').val());
+        var endYear = parseInt($('#srm-form #end-year').val());
+        var endMonth = parseInt($('#srm-form #end-month').val());
+
+        var plansPeriods = generatePlanPeriods(startYear, startMonth, endYear, endMonth);
+
+        var productRow = "<tr>";
+
+        productRow += "<td class='col-lg-1'>" + ($('#srm-form #products-info-table > tbody > tr').length + 1) + "</td>";
+        productRow += getProductDetailsForSRMCellData();
+        productRow +=
+            "<td class='col-lg-8'>" +
+                "<h4 class='centeredText'>Strategic</h4>" +
+                "<div class='tableDiv'>" + buildStrategicPlanTable(plansPeriods) + "</div>" +
+                "<h4 class='centeredText'>Perspective</h4>" +
+                "<div class='tableDiv'>" + buildPerspectivePlanTable(plansPeriods) + "</div>" +
+                "<h4 class='centeredText'>Operative</h4>" +
+                "<div class='tableDiv'>" + buildOperativePlanTable(plansPeriods) + "</div>" +
+            "</td>";
+
+        productRow += "</tr>";
+
+        $('#srm-form #products-info-table > tbody').append(productRow);
 
     });
 });
@@ -68,16 +102,16 @@ function generatePlanPeriods(startYear, startMonthNumber, endYear, endMonthNumbe
 }
 
 function buildStrategicPlanTable(planPeriods) {
-    var table = "<table class='table table-responsive table-bordered'><thead><tr>";
+    var table = "<table class='table table-responsive table-bordered centeredText'><thead><tr>";
     var quartersRow = "";
     var inputsForQuarters = "<tr>";
 
     for (var year in planPeriods) {
         if (planPeriods.hasOwnProperty(year)) {
-            table += '<th colspan="' + Object.keys(planPeriods[year]).length +'">' + year + '</th>';
+            table += '<th class="centeredText" colspan="' + Object.keys(planPeriods[year]).length +'">' + year + '</th>';
             for (var quarter in planPeriods[year]) {
                 quartersRow += "<td>" + quarter + " quarter</td>";
-                inputsForQuarters += "<td><input type='number'/></td>";
+                inputsForQuarters += "<td><input class='centeredText' type='number' min='0'/></td>";
             }
         }
     }
@@ -91,7 +125,7 @@ function buildStrategicPlanTable(planPeriods) {
 }
 
 function buildPerspectivePlanTable(planPeriods) {
-    var table = "<table class='table table-responsive table-bordered'><thead><tr>";
+    var table = "<table class='table table-responsive table-bordered centeredText'><thead><tr>";
     var quartersRow = "";
     var inputsForMonths = "<tr>";
     var monthsRow = "<tr>";
@@ -102,12 +136,12 @@ function buildPerspectivePlanTable(planPeriods) {
             for (var quarter in planPeriods[year]) {
                 quartersRow += '<td colspan="' + planPeriods[year][quarter].size +'">' + quarter + ' quarter</td>';
                 planPeriods[year][quarter].forEach(function (month) {
-                    inputsForMonths += "<td><input type='number'/></td>";
+                    inputsForMonths += "<td><input class='centeredText' type='number' min='0'/></td>";
                     monthsRow += "<td>" + month + "</td>";
                 });
                 colSpansToAddForYear += planPeriods[year][quarter].size;
             }
-            table += '<th colspan="' + colSpansToAddForYear +'">' + year + '</th>';
+            table += '<th class="centeredText" colspan="' + colSpansToAddForYear +'">' + year + '</th>';
         }
     }
 
@@ -121,7 +155,7 @@ function buildPerspectivePlanTable(planPeriods) {
 }
 
 function buildOperativePlanTable(planPeriods) {
-    var table = "<table class='table table-responsive table-bordered'><thead><tr>";
+    var table = "<table class='table table-responsive table-bordered centeredText'><thead><tr>";
     var quartersRow = "";
     var inputsForDecades = "<tr>";
     var monthsRow = "<tr>";
@@ -136,14 +170,14 @@ function buildOperativePlanTable(planPeriods) {
                     monthsRow += "<td colspan='3'>" + month + "</td>";
                     for (var k = 0; k < 3; k++) {
                         decadesRow += "<td>" + (k+1) + " decade</td>";
-                        inputsForDecades += "<td><input type='number'/></td>";
+                        inputsForDecades += "<td><input class='centeredText' type='number' min='0'/></td>";
                         colSpansToAddForQuarter += 1;
                     }
                 });
                 quartersRow += '<td colspan="' + colSpansToAddForQuarter +'">' + quarter + ' quarter</td>';
                 colSpansToAddForYear += colSpansToAddForQuarter;
             }
-            table += '<th colspan="' + colSpansToAddForYear +'">' + year + '</th>';
+            table += '<th class="centeredText" colspan="' + colSpansToAddForYear +'">' + year + '</th>';
         }
     }
 
@@ -157,7 +191,7 @@ function buildOperativePlanTable(planPeriods) {
     return table;
 }
 
-function getProductDetailsCellData() {
+function getProductDetailsForCRMCellData() {
     return '<td class="col-lg-3">' +
         '<div id="product-details" class="form-horizontal">' +
         '<div class="form-group">' +
@@ -191,6 +225,96 @@ function getProductDetailsCellData() {
         '<option value="piece">piece</option>' +
         '<option value="volume">volume</option>' +
         '</select>' +
+        '</div>' +
+        '</div>' +
+        '<br><h5>Sale Information</h5><br>' +
+        '<div class="form-group">' +
+        '<label for="sale-start-date" class="col-lg-4 col-sm-4 control-label">Start Date</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="date" class="form-control" id="sale-start-date">' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="sale-end-date" class="col-lg-4 col-sm-4 control-label">End Date</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="date" class="form-control" id="sale-end-date">' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="promo" class="col-lg-4 col-sm-4 control-label">Promo, %</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="number" class="form-control" id="promo">' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="promo-price" class="col-lg-4 col-sm-4 control-label">Price with Promo</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="text" class="form-control" id="promo-price">' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="quantity" class="col-lg-4 col-sm-4 control-label">Quantity</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="number" class="form-control" id="quantity">' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="sale-comments" class="col-lg-4 col-sm-4 control-label">Comments</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="text" class="form-control" id="sale-comments">' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</td>';
+}
+
+function getProductDetailsForSRMCellData() {
+    return '<td class="col-lg-3">' +
+        '<div id="product-details" class="form-horizontal">' +
+        '<div class="form-group">' +
+        '<label for="product-name" class="col-lg-4 col-sm-4 control-label">Product Name</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="text" class="form-control" id="product-name">' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="sku" class="col-sm-4 control-label">SKU</label>' +
+        '<div class="col-sm-8">' +
+        '<input type="text" class="form-control" id="sku">' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="currency" class="col-sm-4 control-label">Currency</label>' +
+        '<div class="col-sm-8">' +
+        '<select id="currency">' +
+        '<option value="USD">USD</option>' +
+        '<option value="EUR">EUR</option>' +
+        '<option value="BYN">BYN</option>' +
+        '<option value="RUB">RUB</option>' +
+        '</select>' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="dimension-unit" class="col-sm-4 control-label">Dimension</label>' +
+        '<div class="col-sm-8">' +
+        '<select id="dimension-unit">' +
+        '<option value="kg">kg</option>' +
+        '<option value="piece">piece</option>' +
+        '<option value="volume">volume</option>' +
+        '</select>' +
+        '</div>' +
+        '</div>' +
+        '<br><h5>Immediate Request</h5><br>' +
+        '<div class="form-group">' +
+        '<label for="receiving-date" class="col-lg-4 col-sm-4 control-label">Date of Receiving</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="date" class="form-control" id="receiving-date">' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="quantity" class="col-lg-4 col-sm-4 control-label">Quantity</label>' +
+        '<div class="col-lg-8 col-sm-8">' +
+        '<input type="number" class="form-control" id="quantity">' +
         '</div>' +
         '</div>' +
         '</div>' +
--- app/web/view.py
+++ app/web/view.py
@@ -7,3 +7,13 @@ from app import application
 @application.route('/')
 def index():
     return render_template('index.html')
+
+
+@application.route('/srm')
+def srm_supply():
+    return render_template('forms/srm_supply.html')
+
+
+@application.route('/crm')
+def crm_supply():
+    return render_template('forms/crm_supply.html')
